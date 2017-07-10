92449cb547102a9ca20211166adb1c51cc80fb4a
renanluquete <renanluquete@gmail.com>
2017-07-10 17:04:42 -0300

    jogador andando pelo mapa

--- fogefoge.rb
+++ fogefoge.rb
@@ -1,7 +1,49 @@
 require_relative 'ui'
 
-def joga(nome)
+def calcula_nova_posicao(heroi,direcao)
+	heroi = heroi.dup
+	case direcao
+		when "W"
+			heroi[0] -= 1
+		when "S"
+			heroi[0] += 1
+		when "A"
+			heroi[1] -= 1
+		when "D"
+			heroi[1] += 1
+	end
+	heroi
+end
 
+def encontra_jogador(mapa)
+	caracter_do_heroi = "H"
+	mapa.each_with_index do |linha_atual, linha| 
+		coluna_do_heroi = linha_atual.index caracter_do_heroi
+		if coluna_do_heroi
+			return [linha, coluna_do_heroi]
+		end
+	end
+	#não achei
+end
+
+def le_mapa (numero)
+	arquivo = "mapa#{numero}.txt"
+	texto = File.read(arquivo)
+	mapa = texto.split "\n"
+end
+
+def joga(nome)
+	mapa = le_mapa 1
+
+	while true
+		desenha mapa
+		direcao = pede_movimento
+		heroi = encontra_jogador mapa
+		nova_posicao = calcula_nova_posicao heroi, direcao
+		next if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
+		mapa[heroi[0]][heroi[1]] = " "
+		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+	end
 end
 
 def inicia_fogefoge
